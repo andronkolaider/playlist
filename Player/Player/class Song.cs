5ce6613297ff5a4c441dcbfeef7c314b48466e27
Усманаев Андрей <andrei.usmanaev@gmail.com>
2018-05-29 13:23:49 +0300

    added MediaInfo dll

---
+++ Player/Player/class Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using MediaInfoNET;
+namespace Player
+{
+    class Song
+    {
+        public double Duration { get; }
+        public short Rating { get; }
+        public string Artist { get; }
+        public Song(string _artist,short _rating,double _duration)
+        {
+            Duration = _duration;
+            Rating = _rating;
+            Artist = _artist;
+
+        }
+    }
+}
